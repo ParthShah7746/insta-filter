4440d0d98337b4e5c1696668f4903d9ab241ffbe
Parth <p.parthshah99@gmail.com>
2019-07-17 09:36:49 -0500

    Insta Filter Edit1

--- instafilter.py
+++ instafilter.py
@@ -1,14 +1,21 @@
+#Import image from PIL
 from PIL import Image
 
+#Load images into objects
 base_img = Image.open("elon.jpg")
 img_filter = Image.open("t.jpg")
 
+
+#Set O/P Image size
 size = (3840,2160)
 
 
+#Resize all images to o/p size
 base_img = base_img.resize(size)
 img_filter = img_filter.resize(size)
 
+
+#Split each image into RGB vectors
 r ,g ,b = base_img.split()
 R,G,B = img_filter.split()
 
